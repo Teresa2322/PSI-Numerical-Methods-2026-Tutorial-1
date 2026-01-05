8663b834046f7458d8d8d45230d24defafa7604d
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2026-01-05 10:54:48 -0500

    Added myexp function

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def myexp(x, N=10):
+    """
+    This function computes exp(x) via the Taylor Series using terms up to
+    order N.
+    """
+
+    t = 1.0  # The value of the current term
+    y = 1.0  # The value of exp(x) up to this point
+
+    # We initialize with the 0th order term, so run the loop
+    # for the remaining N terms.
+    for i in range(1, N+1):
+        t *= x/i  # Update the term: tn = x^n / n!
+        y += t    # add tn to y
+
+    # We're done!
+    return y
+
+
 if __name__ == "__main__":
 
     print("Hello, world!")
+
+    print("e(1) with  5 terms is", myexp(1.0, 5))
+    print("e(1) with 10 terms is", myexp(1.0, 10))
+    print("e(1) with 20 terms is", myexp(1.0, 20))
+    print("e(1) with 40 terms is", myexp(1.0, 40))
